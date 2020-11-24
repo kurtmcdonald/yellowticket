e7675abe078864e63a1e1bc516935bf8f1ff6f6d
alfie hiscox <alfie.hiscox@gmail.com>
2020-11-24 11:34:02 +0000

    Added Seeds for User (3), Stores (5), Items (5x5)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :stores
+  has_many :stores, dependent: :destroy
   has_many :items, through: :orders
-  has_many :orders
+  has_many :orders, dependent: :destroy
 end
