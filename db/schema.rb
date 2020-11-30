e0fd3b0058acd5b84511c19a71585d3545774352
Luke Sturgeon <lukesturgeon2@gmail.com>
2020-11-30 16:46:15 +0000

    Algolia search working

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_11_30_103808) do
-ActiveRecord::Schema.define(version: 2020_11_30_103526) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
