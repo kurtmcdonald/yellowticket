413eb26bb122dab39e5d48b31b53cb038ac8a0a5
Dexter Quiambao <dexterquiambao@googlemail.com>
2020-11-30 16:58:10 +0000

    Added sweet alert gem, basic qr code generator on the confirmation page and small css tweaks

--- Gemfile
+++ Gemfile
@@ -21,7 +21,8 @@ gem 'jbuilder', '~> 2.7'
 gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+gem 'rqrcode'
+gem 'rqrcode_png'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 gem 'geocoder'
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,3 +2,6 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
 end
+
+# when we save an order and attrribute changes to true then generate qr code
+# qr code - attach it to the order - attribute qr code or picture attached (cloudinary)
